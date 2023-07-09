ffabb653627e8f65390e6d612bfc81bd7b55a049
Magotgarang <m.guut@alustudent.com>
2023-07-09 16:57:36 +0000

    updated

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -12,12 +12,12 @@ if __name__ == "__main__":
         export to JSON
     """
 
-with open("todo_all_employees.json", "w") as jsonfile:
-json.dump({
-    u.get("id"): [{
-        "task": t.get(title")'
-        "completed": t.get("completed"),
-        "username": u.get(username")
-    } for t in requests.get(url + "todos",
-                          params={"userid": u.get("id")}).json()]
-   for u in users}, jsonfile)
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
