0febf7e9295c11c4965a053f5e80388fa3f10748
Magotgarang <m.guut@alustudent.com>
2023-07-09 16:50:16 +0000

    updated

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-      python script that exports data in the JSON format
+    python script that exports data in the JSON format
 """
 import json
 import requests
 
-if __name__ == "__main__ ":
+if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     users = requests.get(url + "users").json()
     """
